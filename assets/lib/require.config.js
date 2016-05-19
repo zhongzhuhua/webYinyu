afe45c5f767b3805198150ac7c3a36a973802d36
zhongzhuhua <672986035@qq.com>
2016-05-19 18:43:31 +0800

    version 003.003

--- assets/lib/require.config.js
+++ assets/lib/require.config.js
@@ -3,8 +3,8 @@ require.config({
   baseUrl: '/assets/',
   urlArgs: '',
   paths: {
-    'global': 'js/gm/global.js?v=' + myargs + '002',
-    'wechat': 'js/gm/wechat.js?v=' + myargs + '002',
-    'layer': 'lib/layer.mobile/layer/layer.js?v=' + myargs + '002'
+    'global': 'js/gm/global.js?v=' + myargs + '003',
+    'wechat': 'js/gm/wechat.js?v=' + myargs + '003',
+    'layer': 'lib/layer.mobile/layer/layer.js?v=' + myargs + '003'
   }
 });
